52141394ca50ee8bd59610aabef8d8b8fdf63bc8
K4ZED <kenzaathallah.wijaya@gmail.com>
2025-12-30 14:25:22 +0700

    capek gua

--- backend/services/supabase.js
+++ backend/services/supabase.js
@@ -1,14 +1,18 @@
 // backend/services/supabase.js
-import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
-dotenv.config();
+import { createClient } from "@supabase/supabase-js";
 
 export const supabaseAdmin = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
+  {
+    auth: { persistSession: false },
+  }
 );
 
 export const supabaseAnon = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_ANON_KEY,
+  {
+    auth: { persistSession: false },
+  }
 );
